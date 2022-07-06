e6944426384df7d579f27a2afbcb394176812e2b
Leonel Mamani <leomamaniinca@frba.utn.edu.ar>
2022-07-06 12:32:33 -0300

    añadiendo estilos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 
 function App() {
-  const key=['equals','zero','one','two','three','four','five','six','seven','eight','nine','clear','decimal','add','subtract','divide','multiply'];
+  const key=[{'equals':'='},
+             {'zero':'0'},
+             {'nine':'9'},
+             {'eight':'8'},
+             {'seven':'7'},
+             {'six':'6'},
+             {'five':'5'},
+             {'four':'4'},
+             {'three':'3'},
+             {'two':'2'},
+             {'one':'1'},
+             {'clear':'AC'},
+             {'decimal':'.'},
+             {'add':'+'},
+             {'subtract':'-'},
+             {'divide':'/'},
+             {'multiply':'x'}]
   return (
     <div id='calculator'>
     <div id='display'>
@@ -10,7 +26,7 @@ function App() {
       {
         key.map((x)=>{
           return(
-            <button className='key' id={x}></button>
+            <button className='key' id={Object.keys(x)[0]}>{Object.values(x)[0]}</button>
           );
         })
       }
